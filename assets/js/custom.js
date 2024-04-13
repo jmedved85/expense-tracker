af6d7e0a169f0863268b9f1fe51b7fd4e177283b
Jasmin Medved <jmedved85@gmail.com>
2024-04-13 15:13:31 +0200

    account & user create button customized

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,27 @@
-console.log('Hello from assets/js/custom.js 🎉');
+// console.log('Hello from assets/js/custom.js 🎉');
+
+// TODO: Temporarily - Check in SonataUserBundle for the user create button template
+document.addEventListener('DOMContentLoaded', function() {
+    let userCreateButton = document.querySelector('a.sonata-action-element[href="/admin/app/user/create"]');
+
+    if (userCreateButton) {
+        userCreateButton.classList.remove('sonata-action-element');
+        userCreateButton.classList.add('btn', 'btn-success');
+        userCreateButton.style.padding = '6px 12px';
+        userCreateButton.style.color = '#fff';
+        userCreateButton.innerHTML = '&nbsp;&nbsp;Add New';
+
+        let icon = document.createElement('i');
+        icon.className = 'fas fa-plus-circle';
+        icon.setAttribute('aria-hidden', 'true');
+        userCreateButton.prepend(icon);
+
+        userCreateButton.addEventListener('mouseover', function() {
+            userCreateButton.style.backgroundColor = '#008d4c'; // Change to the color you want on hover
+        });
+
+        userCreateButton.addEventListener('mouseout', function() {
+            userCreateButton.style.backgroundColor = ''; // Change back to the original color
+        });
+    }
+});
