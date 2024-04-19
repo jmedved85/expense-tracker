b837b2e4cb66c7c166e23262188331123d8d2959
Jasmin Medved <jmedved85@gmail.com>
2024-04-19 09:56:53 +0200

    general category - entity & admin created

--- src/Entity/GeneralCategory.php
+++ src/Entity/GeneralCategory.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\GeneralCategoryRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GeneralCategoryRepository::class)]
@@ -17,21 +15,6 @@ class GeneralCategory
 
     #[ORM\Column(length: 100)]
     private ?string $name = null;
-
-    #[ORM\OneToMany(targetEntity: BudgetItem::class, mappedBy: 'generalCategory')]
-    private Collection $budgetItems;
-
-    #[ORM\ManyToOne(inversedBy: 'generalCategories')]
-    private ?Unit $unit = null;
-
-    #[ORM\OneToMany(targetEntity: PurchaseLine::class, mappedBy: 'generalCategory')]
-    private Collection $purchaseLines;
-
-    public function __construct()
-    {
-        $this->budgetItems = new ArrayCollection();
-        $this->purchaseLines = new ArrayCollection();
-    }
 
     public function __toString()
     {
@@ -51,78 +34,6 @@ class GeneralCategory
     public function setName(string $name): static
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, BudgetItem>
-     */
-    public function getBudgetItems(): Collection
-    {
-        return $this->budgetItems;
-    }
-
-    public function addBudgetItem(BudgetItem $budgetItem): static
-    {
-        if (!$this->budgetItems->contains($budgetItem)) {
-            $this->budgetItems->add($budgetItem);
-            $budgetItem->setGeneralCategory($this);
-        }
-
-        return $this;
-    }
-
-    public function removeBudgetItem(BudgetItem $budgetItem): static
-    {
-        if ($this->budgetItems->removeElement($budgetItem)) {
-            // set the owning side to null (unless already changed)
-            if ($budgetItem->getGeneralCategory() === $this) {
-                $budgetItem->setGeneralCategory(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getUnit(): ?Unit
-    {
-        return $this->unit;
-    }
-
-    public function setUnit(?Unit $unit): static
-    {
-        $this->unit = $unit;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, PurchaseLine>
-     */
-    public function getPurchaseLines(): Collection
-    {
-        return $this->purchaseLines;
-    }
-
-    public function addPurchaseLine(PurchaseLine $purchaseLine): static
-    {
-        if (!$this->purchaseLines->contains($purchaseLine)) {
-            $this->purchaseLines->add($purchaseLine);
-            $purchaseLine->setGeneralCategory($this);
-        }
-
-        return $this;
-    }
-
-    public function removePurchaseLine(PurchaseLine $purchaseLine): static
-    {
-        if ($this->purchaseLines->removeElement($purchaseLine)) {
-            // set the owning side to null (unless already changed)
-            if ($purchaseLine->getGeneralCategory() === $this) {
-                $purchaseLine->setGeneralCategory(null);
-            }
-        }
 
         return $this;
     }
