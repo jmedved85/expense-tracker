e4f6b4873a519e71519c0fd53d19e5317e267d32
Jasmin Medved <jmedved85@gmail.com>
2024-04-23 09:54:29 +0200

    department - entity & admin created

--- src/Entity/Department.php
+++ src/Entity/Department.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\DepartmentRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DepartmentRepository::class)]
@@ -17,17 +15,6 @@ class Department
 
     #[ORM\Column(length: 100)]
     private ?string $name = null;
-
-    #[ORM\ManyToOne(inversedBy: 'departments')]
-    private ?Unit $unit = null;
-
-    #[ORM\OneToMany(targetEntity: Purchase::class, mappedBy: 'department')]
-    private Collection $purchases;
-
-    public function __construct()
-    {
-        $this->purchases = new ArrayCollection();
-    }
 
     public function __toString()
     {
@@ -47,48 +34,6 @@ class Department
     public function setName(string $name): static
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    public function getUnit(): ?Unit
-    {
-        return $this->unit;
-    }
-
-    public function setUnit(?Unit $unit): static
-    {
-        $this->unit = $unit;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Purchase>
-     */
-    public function getPurchases(): Collection
-    {
-        return $this->purchases;
-    }
-
-    public function addPurchase(Purchase $purchase): static
-    {
-        if (!$this->purchases->contains($purchase)) {
-            $this->purchases->add($purchase);
-            $purchase->setDepartment($this);
-        }
-
-        return $this;
-    }
-
-    public function removePurchase(Purchase $purchase): static
-    {
-        if ($this->purchases->removeElement($purchase)) {
-            // set the owning side to null (unless already changed)
-            if ($purchase->getDepartment() === $this) {
-                $purchase->setDepartment(null);
-            }
-        }
 
         return $this;
     }
