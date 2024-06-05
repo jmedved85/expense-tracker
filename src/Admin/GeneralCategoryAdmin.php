61e38a99f6aa5c590b81ee17176ce511612f37f1
Jasmin Medved <jmedved85@gmail.com>
2024-06-05 11:07:05 +0200

    GeneralCategoryAdmin - Update

--- src/Admin/GeneralCategoryAdmin.php
+++ src/Admin/GeneralCategoryAdmin.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace App\Admin;
 
+use App\Entity\GeneralCategory;
+use App\Repository\UnitRepository;
+use InvalidArgumentException;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridInterface;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQuery;
 
 final class GeneralCategoryAdmin extends AbstractAdmin
 {
@@ -30,41 +35,161 @@ final class GeneralCategoryAdmin extends AbstractAdmin
     // MARK: - Datagrid Filters
     protected function configureDatagridFilters(DatagridMapper $filter): void
     {
+        // /* Get unit */
+        // $unitId = $this->getUnitId();
+
         $filter
             ->add('name')
         ;
+
+        // /* If unit is not selected, unit selection is available */
+        // if (!$unitId) {
+        //     $filter
+        //         ->add('unit', null, [
+        //             'label' => 'Unit',
+        //             'show_filter' => true,
+        //             'field_type' => EntityType::class,
+        //             'field_options' => [
+        //                 'class' => Unit::class,
+        //                 'choice_label' => 'name',
+        //                 'query_builder' => function (EntityRepository $er) {
+        //                     return $er->createQueryBuilder('s')
+        //                         ->andWhere('s.active = :active')
+        //                         ->orderBy('s.name', 'ASC')
+        //                         ->setParameter('active', true)
+        //                     ;
+        //                 },
+        //             ],
+        //         ])
+        //     ;
+        // }
     }
 
     // MARK: - List Fields
     protected function configureListFields(ListMapper $list): void
     {
+        // /* Get unit */
+        // $unitId = $this->getUnitId();
+
+        $actions = [
+            'show' => [
+                'template' => 'CRUD/list__action_show_custom.html.twig',
+            ],
+            'edit' => [
+                'template' => 'CRUD/list__action_edit_custom.html.twig',
+            ],
+            'delete' => [
+                'template' => 'CRUD/list__action_delete_custom.html.twig',
+            ],
+        ];
+
         $list
-            ->add('name')
+            ->addIdentifier('name')
+            ->add('unit')
             ->add(ListMapper::NAME_ACTIONS, null, [
-                'actions' => [
-                    'show' => [],
-                    'edit' => [],
-                    'delete' => [],
-                ],
-            ]);
+                'header_style' => 'width: 25%;',
+                'actions' => $actions,
+            ])
+        ;
+
+        // if (!$unitId) {
+        //     $list
+        //         ->add('unit.name', null, [
+        //             'label' => 'Unit'
+        //         ])
+        //     ;
+        // }
     }
 
     // MARK: - Form Fields
     protected function configureFormFields(FormMapper $form): void
     {
+        // /* Get unit */
+        // $unitId = $this->getUnitId();
+
         $form
             ->with('General Category', ['class' => 'col-md-6'])
                 ->add('name')
+                ->add('unit')
             ->end()
         ;
+
+        // if (!$unitId) {
+        //     $form
+        //         ->add('unit', EntityType::class, [
+        //             'label' => 'Unit',
+        //             'class' => Unit::class,
+        //             'choice_label' => 'name',
+        //             'placeholder' => 'Choose an option',
+        //             'query_builder' => function (EntityRepository $er) {
+        //                 return $er->createQueryBuilder('s')
+        //                     ->andWhere('s.active = :active')
+        //                     ->orderBy('s.name', 'ASC')
+        //                     ->setParameter('active', true)
+        //                 ;
+        //             },
+        //         ])
+        //     ;
+        // }
     }
 
     // MARK: - Show Fields
     protected function configureShowFields(ShowMapper $show): void
     {
         $show
-            ->add('name')
+            ->with('Department', ['class' => 'col-md-6'])
+                ->add('name')
+                ->add('unit.name', null, [
+                    'label' => 'Unit'
+                ])
+            ->end()
         ;
+    }
+
+    // MARK: - PrePersist
+    protected function prePersist(object $object): void
+    {
+        /** @var GeneralCategory $object */
+
+        // /** @var UnitRepository $unitRepository */
+        // $unitRepository = $this->entityManager->getRepository(Unit::class);
+
+        // /* Get unit */
+        // $unitId = $this->getUnitId();
+
+        // if ($unitId) {
+        //     $unit = $unitRepository->findOneBy(['id' => $unitId]);
+        //     $account->setUnit($unit);
+        // }
+    }
+
+    // MARK: - Configure Query
+    protected function configureQuery(ProxyQueryInterface $query): ProxyQueryInterface
+    {
+        if (!$query instanceof ProxyQuery) {
+            throw new InvalidArgumentException('Expected an instance of ProxyQuery');
+        }
+
+        // $qb = $query->getQueryBuilder();
+        // $rootAlias = current($query->getQueryBuilder()->getRootAliases());
+
+        // /* Get unit */
+        // $unitId = $this->getUnitId();
+
+        // if ($unitId) {
+        //     $qb
+        //         ->where($rootAlias . '.unit = :unitId')
+        //         ->setParameter('unitId', $unitId)
+        //     ;
+        // } else {
+        //     $qb
+        //         ->join($rootAlias . '.unit', 's')
+        //         ->andWhere('s.active = :active')
+        //         ->setParameter('active', true)
+        //     ;
+        // }
+
+        return $query;
     }
 
     protected function configureDefaultSortValues(array &$sortValues): void
